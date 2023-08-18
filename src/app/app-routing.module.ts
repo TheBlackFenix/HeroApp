5fb25fc4884e81ef05f8a157692b2e66e03e5fb0
TheBlackFenix <danielh1991@hotmail.com>
2023-08-18 16:37:26 -0500

    creacion de rutas, configuracion del routing, creacion de modulos para la siguiente seccion.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'auth',
+    loadChildren: () => import('./auth/auth.module').then(auth => auth.AuthModule)
+  },
+  {
+    path:'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(heroes => heroes.HeroesModule)
+  },
+  {
+    path:'404',
+    component: Error404PageComponent
+  },
+  {
+    path:'',
+    redirectTo: 'heroes',
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component: Error404PageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
